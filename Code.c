5edde2626eab9d3e170af2800705b770e8b64977
aaditya-biswas <147297053+aaditya-biswas@users.noreply.github.com>
2024-04-03 20:06:35 +0530

    Majorly commentation

--- Code.c
+++ Code.c
@@ -26,20 +26,38 @@ void rem_newline(char * str)
 void Read_Users();
 void Read_Users()
 {
-    char c;
-    FILE * read_records;
-    read_records = fopen("Users.txt","r");// Opening user txt file
-    c = fgetc(read_records);
-    printf("\n");
-    while (c != EOF);
+    int count = 1,i=0;
+    char c,str[1500];
+    FILE * read_users;
+    read_users = fopen("Users.txt","r");// Opening user txt file
+    printf("1.");
+    c = fgetc(read_users);
+    while (c != EOF)
     {
-        if (c == '/')// Reading a record
-            printf("|");
+        if (c == '/')
+        {
+            while (c != '\n' && c != EOF)
+            {
+                c = fgetc(read_users);
+            }
+            c = fgetc(read_users);
+            if (c != ' ' && c !=  EOF)
+            {
+                count++;
+                printf("\n%d.%c",count,c);
+                i++;
+            }
+            c = fgetc(read_users);
+        }
         else
-            printf(c);
-        c = fgetc(read_records);
+        {
+            printf("%c",c);
+            c = fgetc(read_users);
+        }
     }
+    printf("\n");
 }
+
 int valid_str(char str[100]);
 int valid_str(char str[100])
 {
@@ -66,7 +84,7 @@ int main()
 {
     int new_user = 0; //Checking if a new user is present or not
     char c,str1[100],str2[100]; // String constant
-    FILE * user_ptr, * records_ptr; // File pointers to each database.
+    FILE * user_ptr, * records_ptr; // File pointers to each database. Check Users.txt for more details
     printf("------------------------------------------------------LIBRARY MANAGEMENT SYSTEM--------------------------------------------------------------------");
     printf("1.New User\n2.Old User\n");
     switch (getchar()) // Choice by numbering.
@@ -88,24 +106,25 @@ int main()
             Add_User(str1,str2); // Function for adding a new user in User.txt
             printf("Added User Successfully!");
             new_user = 1;
-
             break;
         }
         case '2':
         {
+            printf("The existing users are:\n");
+            Read_Users();
             user_ptr = fopen("Users.txt","r");
-            printf("Enter User Id:\n");
-            fgets(str1,100,stdin);
+            printf("Enter User Id:\n"); 
+            fgets(str1,100,stdin); // Taking input str1 which will take user id
             printf("Enter Password:\n");
-            fgets(str2,100,stdin);
-            if (valid_str(str1) && valid_str(str2))
+            fgets(str2,100,stdin); // Taking input str2 which is password to check
+            if (valid_str(str1) && valid_str(str2)) // Validating the string by checking for / character (/ - separator)
             {
-                rem_newline(str1);
+                rem_newline(str1); // removing newline from fgets
                 rem_newline(str2);
-                if (Check_User(str1,str2) == 1)
+                if (Check_User(str1,str2) != 0) //  Function Check_User will return non - zero value if User _id & Password match else it will return 0
                 {
                     printf("\n WELCOME %s!\n",str1);
-                    fseek(user_ptr,User_teller(str1),SEEK_SET);
+                    fseek(user_ptr,Book_User_pos(str1),SEEK_SET); // Book_User_pos to set the  file pointer to the Books locaation
                 }
                 else 
                 {
