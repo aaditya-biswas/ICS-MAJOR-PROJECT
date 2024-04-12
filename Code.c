dca299bf4ce013adf6d65712215c83c64e305f3f
aaditya-biswas <147297053+aaditya-biswas@users.noreply.github.com>
2024-04-12 20:26:17 +0530

    Major changes

--- Code.c
+++ Code.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
 
 typedef struct {
@@ -14,19 +15,92 @@ typedef struct {
     char DOB[15];
     char Issued_Books[1500];
 } User;
-void Read_User_Books(FILE * fptr);
-void Read_User_Books(FILE * fptr)
+
+int Borrow_Book();
+void exit_func();
+void Read_User_Books(int n);                               // Function to read Books of user
+int Add_User();              // Function to Add New User to the database
+void rem_newline( char * str);                                 // Function to remove newline 
+int Check_User();                         
+void Read_Users();
+int valid_str(char str[100]);
+void print_Book(char * Book_name);
+int Book_Search();
+Book get_book(int n);
+
+int Borrow_Book()
 {
-    char c = fgetc(fptr);
-    printf("\n");
-    while (c!='\n' && c!=EOF)
-    {
-        printf("%c",c);
-        c = fgetc(fptr);
-    }
 
 }
-void rem_newline( char * str); // Function to remove newline 
+
+void exit_func()
+{
+    printf("Thank you for using our Library Management System. Exiting");
+    exit(0);
+}
+
+void Read_User_Books(int n)
+{
+    FILE * user_data;
+    user_data = fopen("Users.txt","r");
+    char c,str[1500];
+    int count = 0;
+    while (n)
+    {
+        fgets(str,1500,user_data);
+        n--;
+    }
+    c = fgetc(user_data);
+    while (c != EOF && c != '\n')
+    {
+        if (c == '/')
+        {
+            count++;
+        }
+        if (count > 2)
+        {
+            printf("%c",c);
+        }
+        c = fgetc(user_data);
+    }
+    fclose(user_data);
+
+}
+int Add_User() // Please follow this outline to design a function
+{
+    fflush(stdin);
+    FILE * user_ptr = fopen("Users.txt","a");
+    char c,user_name[100],pass_word[100],DOB[100];
+    printf("Enter Username:\n");
+    fgets(user_name,100,stdin);
+    printf("Enter Password:\n");
+    fgets(pass_word,100,stdin);
+    printf("Enter Date Of Birth:\n");
+    fgets(DOB,100,stdin);
+    rem_newline(user_name);
+    rem_newline(pass_word);
+    rem_newline(DOB);
+    if (valid_str(user_name) && valid_str(pass_word) && valid_str(DOB))
+    {
+        fprintf(user_ptr,"\n%s/%s/%s/",user_name,pass_word,DOB);
+        printf("Added User Successfully!");
+        fclose(user_ptr);
+    }
+    else
+    {
+        fclose(user_ptr);
+        printf("Invalid details entered.Would you like to try again? Y or N");
+        if (getchar() == 'Y')
+        {
+            Add_User();
+        } 
+        else
+        {
+           exit_func();
+        }
+    }
+}
+
 void rem_newline(char * str)
 {
     char * ptr = str;
@@ -36,7 +110,7 @@ void rem_newline(char * str)
     }
     *ptr = '\0';
 }
-Book get_book(int n);
+
 Book get_book(int n)
 {
     int i = 0,qty,count = 0;
@@ -99,11 +173,18 @@ Book get_book(int n)
         }
     }  
 }
-int Check_User(char * username,char * password);
-int Check_User(char * username,char * password)
+
+int Check_User()
 {
-    int count = 0,i=0,num = 1,check = 0;
-    char c,user_name[100],pass_word[100],str3[1500];
+    fflush(stdin);
+    int count = 0,i=0,num = 0,check = 0;
+    char c,user_name[100],username[100],password[100],pass_word[100],str3[1500];
+    printf("Enter Username\n");
+    fgets(username,100,stdin);
+    printf("Enter Password\n");
+    fgets(password,100,stdin);
+    rem_newline(username);
+    rem_newline(password);
     FILE * fptr = fopen("Users.txt","r");
     c = fgetc(fptr);
     while (!feof(fptr))
@@ -112,7 +193,7 @@ int Check_User(char * username,char * password)
         {
             count++;
         }
-        switch (count)
+        switch (count )
         {
             case 0:
             {
@@ -160,11 +241,9 @@ int Check_User(char * username,char * password)
                     c = fgetc(fptr);
                     break;
                 }
-                
-                
+                break;   
             }
         }
-
     }
     fclose(fptr);
     if (check == 1)
@@ -173,10 +252,18 @@ int Check_User(char * username,char * password)
     }
     else
     {
-        return 0;
+        printf("Invalid Input !\n Do you want to retry? Y or N");
+        if (getchar() == 'Y')
+        {
+            Check_User();
+        }
+        else
+        {
+            exit_func();
+        }
     }
 }
-int Book_User_pos(int n,FILE * fptr);
+
 int Book_User_pos(int n,FILE * fptr)
 {
     int count = 0;
@@ -201,7 +288,7 @@ int Book_User_pos(int n,FILE * fptr)
 
     }
 }
-void Read_Users();
+
 void Read_Users()
 {
     int count = 1,i=0;
@@ -236,7 +323,7 @@ void Read_Users()
     printf("\n");
 }
 
-int valid_str(char str[100]);
+
 int valid_str(char str[100])
 {
     int i = 0;
@@ -253,68 +340,40 @@ int valid_str(char str[100])
     return 1;
 
 }
+
+int Book_Search()
+{
+    ;
+}
+
 int main()
 {
-    int new_user = 0; //Checking if a new user is present or not
+    int new_user = 0,user_pos; //Checking if a new user is present or not
     char c,user_name[100],pass_word[100],str3[1500]; // String constant
-    FILE * user_ptr, * records_ptr; // File pointers to each database. Check Users.txt for more details
-    printf("------------------------------------------------------LIBRARY MANAGEMENT SYSTEM--------------------------------------------------------------------");
+    printf("------------------------------------------------------LIBRARY MANAGEMENT SYSTEM--------------------------------------------------------------------\n");
     printf("1.New User\n2.Old User\n");
     switch (getchar()) // Choice by numbering.
     {
         case '1':
         {
-            printf("Enter Username:\n");
-            fgets(user_name,100,stdin);
-            while (valid_str(user_name) == 0) // Checks for a valid string . We have used '/' as delimeter .
-            {
-                fgets(user_name,100,stdin);
-            }
-            printf("Enter Password:\n");
-            fgets(pass_word,100,stdin);
-            while (valid_str(pass_word) == 0) // Checks for a valid string . We have used '/' as delimeter .
-            {
-                fgets(pass_word,100,stdin);
-            }
-            Add_User(user_name,pass_word); // Function for adding a new user in User.txt
-            printf("Added User Successfully!");
+            Add_User();
             new_user = 1;
             break;
         }
         case '2':
         {
-            printf("The existing users are:\n");
             Read_Users();
-            user_ptr = fopen("Users.txt","r");
-            printf("Enter User Id:\n"); 
-            fgets(user_name,100,stdin); // Taking input user_name which will take user id
-            printf("Enter Password:\n");
-            fgets(pass_word,100,stdin); // Taking input pass_word which is password to check
-            if (valid_str(user_name) && valid_str(pass_word)) // Validating the string by checking for / character (/ - separator)
-            {
-                rem_newline(user_name); // removing newline from fgets
-                rem_newline(pass_word);
-                if (Check_User(user_name,pass_word) != 0) //  Function Check_User will return non - zero value if User _id & Password match else it will return 0
-                {
-                    printf("\n WELCOME %s!\n",user_name);
-                    fseek(user_ptr,Book_User_pos(Check_User(user_name,pass_word),user_ptr),SEEK_SET); // Book_User_pos to set the  file pointer to the Books locaation
-                }
-                else 
-                {
-                    printf("Invalid Username or Password");
-                    exit(0);
-                }
-            }
+            user_pos = Check_User();
             break;
         }
         default:
         {
             printf("\nPlease enter 1 or 2\n");
-            main();
+            exit_func();
             break;
         }
     }
-    printf("\nChoose from the options below:\n1.View Borrowed Books.\n2.Borrow Books.\n3.Return Books.\n4.Edit Account Details\n5.Delete Account\n");
+    printf("\nChoose from the options below:\n1.View Borrowed Books.\n2.Borrow Books.\n3.Return Books.\n4.Edit Account Details\n5.Delete Account\n6.Exit\n");
     switch (getchar())
     {
         case '1':
@@ -325,7 +384,7 @@ int main()
             }
             else 
             {
-                Read_User_Books(user_ptr);
+                Read_User_Books(user_pos);
             }
         }
         case '2':
@@ -335,17 +394,47 @@ int main()
             {
             case '1':
             {
-                printf("Enter the book name.\n");
-                fgets(str3,1500,stdin);
-                rem_newline(str3);
-                if (Book_Search()) //Book_Search returns the line number of the book
+                int x = Book_Search();
+                if (x && get_book(x).Quantity > 0)  //Book_Search returns the structure of the book
                 {
                     printf("Yes the book exists and the book details are:\n");
-                    print_book(Book_Search);
+                    printf("Book_Id |   Title   |   Author  |   Quantity\n%s    %s  %s  %d",get_book(x).Book_Id,get_book(x).Title,get_book(x).Author,get_book(x).Quantity);
+                    printf("Would you like to borrow? Y or N");
+                    if (getchar() == 'Y')
+                    {
+                        Borrow_Book();
+                    }
+                    
+                }
+                else
+                {
+                    printf("The book doesn't exist or is out of stock. Sorry for the inconvenience caused.");
+
                 }
                 break;
             }
             case '2':
+            {
+                printf("The list of books are:");
+                printf("Book_Id |  Title     | Author    | Quantity ");
+                {
+                    FILE * Book_list = fopen("Books.txt","r");
+                    c = fgetc(Book_list);
+                    while (c != EOF)
+                    {
+                        if (c == '/')
+                        {
+                            printf("\t");
+                        }
+                        else
+                        {
+                            printf("%c",c);
+                        }
+                        c = fgetc(Book_list);
+                    }
+                    fclose(Book_list);
+                }
+            }
             
             default:
             {
@@ -367,6 +456,10 @@ int main()
         case '5':
         {
 
+        }
+        case '6':
+        {
+            exit_func();
         }
     }
 }
